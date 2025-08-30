863f32cdb840d960015e39c4abae386a0486050a
NipunSGeeTH <sangeethnipun385@gmail.com>
2025-08-30 16:58:17 +0530

    feat: add DaisyUI to Tailwind in SvelteKit project - Installed DaisyUI plugin - Updated tailwind.config.cjs to include DaisyUI - Verified Tailwind + DaisyUI styling works with example components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
-<h1 class="text-3xl font-bold underline text-green-700">Welcome to SvelteKit</h1>
+<div class="flex flex-col items-center justify-center min-h-screen gap-4 text-center">
+  <h1 class="text-3xl font-bold underline text-green-700">
+    Welcome to SvelteKit
+  </h1>
 
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+  <p>
+    Visit <a class="text-blue-600 hover:underline" href="https://svelte.dev/docs/kit">
+      svelte.dev/docs/kit
+    </a> to read the documentation
+  </p>
+
+  <button class="btn btn-soft btn-primary">Test button for Daisy UI</button>
+</div>
+
