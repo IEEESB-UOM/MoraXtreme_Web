29d3096069cc3657bd4c98060773e8086f23d9fb
Suhas Dissanayake <suhasdissa@gmail.com>
2025-09-03 17:27:59 +0530

    fix: mobile view

--- src/components/HeroSection.svelte
+++ src/components/HeroSection.svelte
@@ -1,23 +1,28 @@
-<div class="hero min-h-screen relative overflow-hidden w-screen">
-    <div
-        class="hero-content text-center text-white relative z-10 flex flex-col"
-    >
-        <h1
-            class="text-7xl font-bold mb-8 drop-shadow-lg font-display text-white bg-gradient-to-r from-purple-400 via-blue-300 to-purple-500 bg-clip-text"
-            style="text-shadow: 0 0 20px #8b5cf6, 0 0 40px #3b82f6, 0 0 60px #8b5cf6;"
+<section
+    id="hero"
+    class="text-base-content flex-col px-6 md:px-20 min-h-screen flex items-center justify-center relative"
+>
+    <div class="hero min-h-screen relative overflow-hidden w-screen">
+        <div
+            class="hero-content text-center text-white relative z-10 flex flex-col"
         >
-            MoraXtreme 10.0
-        </h1>
-        <p class="text-3xl mb-8 drop-shadow-md">
-            Sri Lanka's Largest Technology Competition
-        </p>
-        <div class="flex flex-col sm:flex-row gap-4 justify-center">
-            <a
-                href="/register"
-                class="btn btn-primary btn-lg"
-                style="box-shadow: 0 0 20px #8b5cf6ab, 0 0 40px #3b82f670;"
-                >Register Now</a
+            <h1
+                class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-8 drop-shadow-lg font-display text-white bg-gradient-to-r from-purple-400 via-blue-300 to-purple-500 bg-clip-text"
+                style="text-shadow: 0 0 20px #8b5cf6, 0 0 40px #3b82f6, 0 0 60px #8b5cf6;"
             >
+                MoraXtreme 10.0
+            </h1>
+            <p class="text-lg sm:text-xl md:text-2xl lg:text-3xl mb-8 drop-shadow-md">
+                Sri Lanka's Largest Technology Competition
+            </p>
+            <div class="flex flex-col sm:flex-row gap-4 justify-center">
+                <a
+                    href="/register"
+                    class="btn btn-primary btn-lg"
+                    style="box-shadow: 0 0 20px #8b5cf6ab, 0 0 40px #3b82f670;"
+                    >Register Now</a
+                >
+            </div>
         </div>
     </div>
-</div>
+</section>
